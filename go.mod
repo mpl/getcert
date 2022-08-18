1fabf30009ac12d9d8018e17613d8aded2f31667
mpl <mathieu.lonjaret@gmail.com>
2022-08-18 15:58:32 +0200

    update and cleanup

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.13
 require (
 	github.com/davecgh/go-spew v1.1.1
 	github.com/go-acme/lego/v3 v3.1.0
-	golang.org/x/crypto v0.0.0-20191011191535-87dc89f01550
-	golang.org/x/net v0.0.0-20191028085509-fe3aa8a45271
+	golang.org/x/crypto v0.0.0-20220817201139-bc19a97f63c8
+	golang.org/x/net v0.0.0-20220812174116-3211cb980234
+	golang.org/x/sys v0.0.0-20220817070843-5a390386f1f2 // indirect
 )
--- main.go
+++ main.go
@@ -4,29 +4,15 @@
 package main
 
 import (
-	"crypto"
-	"crypto/ecdsa"
-	"crypto/elliptic"
-	"crypto/rand"
 	"crypto/tls"
 	"flag"
-	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"path/filepath"
-	"time"
 
 	"github.com/davecgh/go-spew/spew"
-	"golang.org/x/crypto/acme"
 	"golang.org/x/crypto/acme/autocert"
-	"golang.org/x/net/http2"
-
-	"github.com/go-acme/lego/v3/certcrypto"
-	"github.com/go-acme/lego/v3/certificate"
-	"github.com/go-acme/lego/v3/challenge/tlsalpn01"
-	"github.com/go-acme/lego/v3/lego"
-	"github.com/go-acme/lego/v3/registration"
 )
 
 const stagingURL = "https://acme-staging-v02.api.letsencrypt.org/directory"
@@ -45,29 +31,24 @@ func main() {
 
 	dirCache := filepath.Join(os.Getenv("HOME"), ".cache", "letsencrypt")
 	m := autocert.Manager{
-		Prompt:     autocert.AcceptTOS,
+		Prompt: autocert.AcceptTOS,
+		// TODO(mpl): allow multiple hostnames and/or IPs (i.e. -> SAN)
 		HostPolicy: autocert.HostWhitelist(*flagHostname),
-		// HostPolicy: func(ctx context.Context, host string) error { return nil },
-		Cache: autocert.DirCache(dirCache),
+		Cache:      autocert.DirCache(dirCache),
 	}
 	getCertificate := func(hello *tls.ClientHelloInfo) (*tls.Certificate, error) {
 		if *flagVerbose {
 			spew.Dump(hello)
 		}
-		return m.GetCertificate(hello)
+		cert, err := m.GetCertificate(hello)
+		if err != nil {
+			log.Println(err)
+		}
+		return cert, err
 	}
 
-	//	tlsConfig := m.TLSConfig()
-	//	tlsConfig.GetCertificate = getCertificate
-
-	tlsConfig := &tls.Config{
-		Rand: rand.Reader,
-		Time: time.Now,
-		// NextProtos: []string{http2.NextProtoTLS, "http/1.1", "acme-tls/1"},
-		NextProtos: []string{http2.NextProtoTLS, "http/1.1", acme.ALPNProto},
-		//	   		MinVersion: tls.VersionTLS12,
-		GetCertificate: getCertificate,
-	}
+	tlsConfig := m.TLSConfig()
+	tlsConfig.GetCertificate = getCertificate
 
 	ln, err := tls.Listen("tcp", ":443", tlsConfig)
 	if err != nil {
@@ -77,70 +58,4 @@ func main() {
 		http.Error(w, "nope", 404)
 	})
 	log.Fatal(http.Serve(ln, nil))
-}
-
-// You'll need a user or account type that implements acme.User
-type MyUser struct {
-	Email        string
-	Registration *registration.Resource
-	key          crypto.PrivateKey
-}
-
-func (u *MyUser) GetEmail() string {
-	return u.Email
-}
-func (u MyUser) GetRegistration() *registration.Resource {
-	return u.Registration
-}
-func (u *MyUser) GetPrivateKey() crypto.PrivateKey {
-	return u.key
-}
-
-func dolego() {
-
-	// Create a user. New accounts need an email and private key to start.
-	privateKey, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	myUser := MyUser{
-		Email: "mathieu.lonjaret@gmail.com",
-		key:   privateKey,
-	}
-
-	config := lego.NewConfig(&myUser)
-	// config.CADirURL = stagingURL
-	config.Certificate.KeyType = certcrypto.RSA2048
-
-	// A client facilitates communication with the CA server.
-	client, err := lego.NewClient(config)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	err = client.Challenge.SetTLSALPN01Provider(tlsalpn01.NewProviderServer("", "443"))
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// New users will need to register
-	reg, err := client.Registration.Register(registration.RegisterOptions{TermsOfServiceAgreed: true})
-	if err != nil {
-		log.Fatal(err)
-	}
-	myUser.Registration = reg
-
-	request := certificate.ObtainRequest{
-		Domains: []string{*flagHostname},
-		Bundle:  true,
-	}
-	certificates, err := client.Certificate.Obtain(request)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Each certificate comes back with the cert bytes, the bytes of the client's
-	// private key, and a certificate URL. SAVE THESE TO DISK.
-	fmt.Printf("%#v\n", certificates)
 }
